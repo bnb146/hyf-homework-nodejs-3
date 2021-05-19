abf034174333c0084555d7ed7f7c33e0ce8dd35a
Barbara <bnburkhardt@gmail.com>
2021-05-19 10:46:22 +0200

    implemented rest api easy way Hello World

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+    res.send('Hello World!');
 });
 
 const users = [];
