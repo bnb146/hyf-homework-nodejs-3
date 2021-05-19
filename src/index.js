1eacdd93ae5e272a9d7166367e513834600b6494
Barbara <bnburkhardt@gmail.com>
2021-05-19 10:34:22 +0200

    implemented rest api easy way

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors');
+
 const app = express()
 const port = 3000
-
-//Require the Route defined in router.js
-const router = require('router');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', router);
-app.use('/users', router);
+app.get('/', (req, res) => {
+    res.send('Hello World, from express');
+});
+
+const users = [];
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
